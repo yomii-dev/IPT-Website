884d4f88775e79c2a07ffd93c5aec3c259279bf5
Distinct <distinct3579@gmail.com>
2026-05-27 15:27:50 +0800

    Added frontend design in homepage.

--- templates/index.php
+++ templates/index.php
@@ -1,5 +1,4 @@
 <!--HOME PAGE-->
-
 <!DOCTYPE html>
 
 <html lang="en">
@@ -26,13 +25,115 @@
                 <a href="product.php" class="bg-[#262930] text-gray-300 font-medium px-4 py-1.5 rounded-lg text-xs hover:bg-gray-700">Product</a>
                 <a href="service.php" class="bg-[#262930] text-gray-300 font-medium px-4 py-1.5 rounded-lg text-xs hover:bg-gray-700">Service</a>
             </div>
-
+            <!--In this part, dapat lalabas yung logout tas didirect siya sa login page-->
             <div class="flex items-center pr-2">
                 <img src="/IPT-Website/assets/user_profile.svg" alt="Your Profile" class="h-7 w-auto select-none">
             </div>
 
         </nav>
     </div>
+    
+    <!--MAIN CONTAINER-->
+    <main class="w-full max-w-7xl mx-auto px-6 py-8 space-y-12 mb-10">
+
+        <div class="relative rounded-xl h-[380px]">
+
+            <img src="/IPT-Website/assets/home_bg_card.jpg" alt="BG Image" class="absolute inset-0 w-full h-full object-cover object-center brightness-[0.3]">
+
+            <div class="relative h-full flex flex-col justify-center px-10 px-16 max-w-2xl space-y-4">
+                <h1 class="text-4xl text-[44px] font-black text-white">
+                    Your ultimate setup, piece<br>by piece, on your terms.
+                </h1>
+                <p class="text-[16px] text-gray-400 max-w-xl">
+                    P3R gives you instant access to high-end custom setups. Pick your specs, rent your dream PC set, and conquer any game or workload without the heavy upfront cost.
+                </p>
+
+                <div class="flex flex-wrap gap-4 pt-2">
+                    <a href="product.php" class="bg-transparent text-white border border-gray-700 font-bold text-[14px] 
+                    px-6 py-2 rounded-lg text-gray-300 hover:text-white hover:border-white">
+                        Shop Now
+                    </a>
+                    <a href="#" class="bg-transparent text-white border border-gray-700 font-bold text-[14px] 
+                    px-6 py-2 rounded-lg text-gray-300 hover:text-white hover:border-white">
+                        Learn more
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="space-y-6">
+            <div class="bg-[#252A2E] flex-row rounded-xl p-6 flex flex-col gap-8 items-center">
+                
+                <div class="w-[320px] shrink-0 aspect-square rounded-xl overflow-hidden">
+                    <img src="/IPT-Website/assets/samplepic.jpg" alt="What P3R offers picture" class="w-full h-full object-cover">
+                </div>
+
+                <div class="flex-grow space-y-3 w-full pl-14 text-center text-left">
+                    <h2 class="text-3xl font-black text-white">What does P3R offer?</h2>
+                    <p class="text-xs text-[18px] text-gray-400">
+                        P3R lets consumers buy PCs / PC parts and rent PCs based on their needs and preferences for work, 
+                        gaming, or creative tasks. It helps users choose components, build their preferred setup, and access 
+                        performant computers without full purchase cost.
+                    </p>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+
+            <div class="bg-[#252A2E] rounded-xl p-6 flex flex-col items-center text-center space-y-3">
+                <div class="w-16 h-16 flex items-center justify-center">
+                    <img src="/IPT-Website/assets/test.png" alt="Test Approved Icon" class="w-full h-full object-contain">
+                </div>
+                <h3 class="text-2xl font-black text-white uppercase">
+                    Rigorous<br>Testing
+                </h3>
+                <p class="text-[16px] text-gray-400">
+                    Every component is stress-tested before dispatch for guaranteed stability.
+                </p>
+            </div>
+
+            <div class="bg-[#252A2E] rounded-xl p-6 flex flex-col items-center text-center space-y-3">
+                <div class="w-16 h-16 flex items-center justify-center">
+                    <img src="/IPT-Website/assets/custom.png" alt="Custom Build Icon" class="w-full h-full object-contain">
+                </div>
+                <h3 class="text-2xl font-black text-white uppercase">
+                    Custom<br>Builder
+                </h3>
+                <p class="text-[16px] text-gray-400">
+                    Select each individual component piece-by-piece for ultimate control over your setup.
+                </p>
+            </div>
+
+            <div class="bg-[#252A2E] rounded-xl p-6 flex flex-col items-center text-center space-y-3">
+                <div class="w-16 h-16 flex items-center justify-center">
+                    <img src="/IPT-Website/assets/cost.png" alt="Cost-To-Performance Icon" class="w-full h-full object-contain">
+                </div>
+                <h3 class="text-2xl font-black text-white uppercase">
+                    Cost-To-<br>Performance
+                </h3>
+                <p class="text-[16px] text-gray-400">
+                    Compare side-by-side technical specs against real-time pricing to find the best hardware value.
+                </p>
+            </div>
+
+            <div class="bg-[#252A2E] rounded-xl p-6 flex flex-col items-center text-center space-y-3">
+                <div class="w-16 h-16 flex items-center justify-center">
+                    <img src="/IPT-Website/assets/time.png" alt="Maximum Flexibility Icon" class="w-full h-full object-contain">
+                </div>
+                <h3 class="text-2xl font-black text-white uppercase">
+                    Maximum<br>Flexibility
+                </h3>
+                <p class="text-[16px] text-gray-400">
+                    Rent by day, week, or month on your own terms.
+                </p>
+            </div>
+
+        </div>
+
+    </main>
    
     <!--FOOTER LAYOUT-->
     <footer class="bg-[#050608] w-full pt-16 pb-6 px-8 md:px-16 text-[13px] text-gray-400 font-normal">
@@ -75,9 +176,9 @@
                 <div class="space-y-4 min-w-[100px]">
                     <h4 class="text-white font-bold text-[14px]">Quick links</h4>
                     <ol class="space-y-3 text-gray-400">
-                        <li><a href="#" class="hover:text-white transition">Home</a></li>
-                        <li><a href="#" class="hover:text-white transition">Product</a></li>
-                        <li><a href="#" class="hover:text-white transition">Service</a></li>
+                        <li><a href="index.php" class="hover:text-white transition">Home</a></li>
+                        <li><a href="product.php" class="hover:text-white transition">Product</a></li>
+                        <li><a href="service.php" class="hover:text-white transition">Service</a></li>
                     </ol>
                 </div>
 
